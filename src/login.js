7b32319291c4dc2b230f93e03eecc84fe6293663
Ehsan Asdar <ehsanmasdar@gmail.com>
2016-02-07 11:55:24 -0600

    Add more detailed messaging

--- src/login.js
+++ src/login.js
@@ -1,4 +1,15 @@
+function getURLParameter(name) {
+  return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search)||[,""])[1].replace(/\+/g, '%20'))||null
+}
 $(function() {
+  if (getURLParameter('v') == 1){
+    $('#helpBlock').text("Account Created! Check your email for verification instructions.");
+    $('#helpBlock').css({"color":"#00C853"});
+  }
+  else if (getURLParameter('s') == 1){
+    $('#inputGroup').addClass('has-failure');
+    $('#helpBlock').text("You must sign in to access that page.")
+  }
   $( "#login-form" ).submit(function( event ) {
     event.preventDefault();
     var username = $('#login-form').find('input[id="inputUsername"]').val();
