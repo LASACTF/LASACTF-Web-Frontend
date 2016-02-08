9682063ef650e2fab42ce47524100302d7b56d50
Ehsan Asdar <ehsanmasdar@gmail.com>
2016-02-08 16:03:32 -0600

    Almost finish profile page

--- src/profile.js
+++ src/profile.js
@@ -1,36 +1,81 @@
-function resize()
-{
-  var width = $(this).width(); //this = window
-  var posX = width - ($(".navbar-container").width());
-  //$(".compete#left-panel").css("width", posX);
-  $(".compete#left-panel button").css("padding-left", posX / 2 + 14);
-}
-
-$(window).on('load', function(){
-  resize();
-});
-$(window).on('resize', function(){
-  resize();
-});
-
 $(function() {
   $.ajax({
      url: '/api/user/status',
-     success: function(data) {
-      console.log(data)
+     success: function(result) {
+       console.log(result);
+       var extra = JSON.parse(result.data['extra']);
+       console.log(extra);
+       if (result.status == 1){
+         $('#textUsername').text(result.data['username']);
+         $('#textName').text(result.data['firstname'] + " " + result.data['lastname']);
+         $('#textEmail').text(result.data['email']);
+         $('#textAffliation').text(extra['affiliation']);
+         switch (extra.schooltype) {
+           case "high":
+             $('#textAffliationType').text("High School or Equivalent");
+             break;
+           case "college":
+             $('#textAffliationType').text("Higher Education (college, university, etc.)");
+             break;
+           case "corp":
+             $('#textAffliationType').text("Corporation / Non-Profit");
+             break;
+          case "middle":
+             $('#textAffliationType').text("Middle School or Equivalent");
+             break;
+          case "other":
+             $('#textAffliationType').text("Other");
+             break;
+           default:
+             $('#textAffliationType').text("High School or Equivalent");
+             break;
+         }
+         if(extra['state'].length > 0){
+           $('#textLocation1').text(extra['city'] + ", " + extra['state']);
+         }
+         else{
+           $('#textLocation1').text(extra['city']);
+         }
+         if (extra['country'] == 'us'){
+           $('#textLocation2').text("United States");
+         }
+         else{
+           $('#textLocation2').text(extra['international']);
+         }
+         if(extra['eligibility'] == "eligible"){
+           $('#textEligibile').text('Eligible').addClass("success-text")
+         }
+         else{
+           $('#textEligibile').text('Not Eligible').addClass("failure-text")
+         }
+         if(result.data['team_name'] != result.data['username']){
+           $('inputTeam').text(result.data['team_name']);
+         }
+
+       }
      },
      type: 'GET'
   });
-  $( ".dropdown-toggle" ).hover(
-    function(){
-      $('.dropdown-menu').slideDown(200);
-      setTimeout(function() { $('.navbar .nav li.button ul.user-dropdown .btn').css("opacity", "1"); }, 250);
-
-    },
-    function(){
-      $('.navbar .nav li.button ul.user-dropdown .btn').css("opacity", "0");
-      setTimeout(function() { $('.dropdown-menu').slideUp(200); }, 250);
-
-    }
-  );
+  $( "#actionJoin" ).click(function() {
+    var team_name = $('#inputTeam').val()
+    var team_password = $('#inputTeamPass').val()
+    $.ajax({
+       url: '/api/team/join',
+       data: {
+          "team_name": team_name,
+          "team_password": team_password,
+       },
+       success: function(data) {
+        if (data.status == 1){
+          window.location.href = "/profile";
+        }
+        else{
+          $('#helpBlock').removeClass("success-text");
+          $('#inputGroup').addClass('has-failure');
+          $('#helpBlock').text(data.message);
+        }
+       },
+       type: 'POST'
+    });
+  });
 });
