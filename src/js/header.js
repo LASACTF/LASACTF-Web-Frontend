33ddf5de862809072a38f58f9c03864796e1db2c
Ehsan Asdar <ehsanmasdar@gmail.com>
2016-02-19 11:43:43 -0600

    Add timer after competition start

--- src/js/header.js
+++ src/js/header.js
@@ -64,12 +64,23 @@ $(function() {
         }, 250);
       }
     );
-    var timespan = countdown(new Date("March 19, 2016 12:00:00 CST"), function(ts) {
-      $('#tdays').text(padDigits(ts.days,2));
-      $('#thours').text(padDigits(ts.hours,2));
-      $('#tmins').text(padDigits(ts.minutes,2));
-      $('#tsecs').text(padDigits(ts.seconds,2));
-    }, countdown.DAYS | countdown.HOURS|countdown.MINUTES|countdown.SECONDS|countdown.MILLISECONDS);
+    if(Math.floor(Date.now() / 1000) < 1458406800){
+      var timespan = countdown(new Date("March 19, 2016 12:00:00 CDT"), function(ts) {
+        $('#tdays').text(padDigits(ts.days,2));
+        $('#thours').text(padDigits(ts.hours,2));
+        $('#tmins').text(padDigits(ts.minutes,2));
+        $('#tsecs').text(padDigits(ts.seconds,2));
+      }, countdown.DAYS | countdown.HOURS|countdown.MINUTES|countdown.SECONDS|countdown.MILLISECONDS);
+    }
+    else{
+      $('#ttext').text(' left');
+      var timespan = countdown(new Date("March 26, 2016 12:00:00 CDT"), function(ts) {
+        $('#tdays').text(padDigits(ts.days,2));
+        $('#thours').text(padDigits(ts.hours,2));
+        $('#tmins').text(padDigits(ts.minutes,2));
+        $('#tsecs').text(padDigits(ts.seconds,2));
+      }, countdown.DAYS | countdown.HOURS|countdown.MINUTES|countdown.SECONDS|countdown.MILLISECONDS);
+    }
 
     if (localStorage.getItem("nocountdown") == "true"){
       $('#countdown').addClass("hidden");
