6504585a7fbb3f84f36060db3c4d14659aa05002
Ehsan Asdar <ehsanmasdar@gmail.com>
2016-02-20 10:16:34 -0600

    Refactor a lot of JS

--- src/js/login.js
+++ src/js/login.js
@@ -1,38 +1,36 @@
 function getURLParameter(name) {
-  return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search)||[,""])[1].replace(/\+/g, '%20'))||null
+  return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search) || [, ""])[1].replace(/\+/g, '%20')) || null;
 }
 $(function() {
-  if (getURLParameter('v') == 1){
+  if (getURLParameter('v') == 1) {
     $('#helpBlock').addClass("success-text");
     $('#helpBlock').text("Account Created! Check your email for verification instructions.");
-  }
-  else if (getURLParameter('s') == 1){
+  } else if (getURLParameter('s') == 1) {
     $('#inputGroup').addClass('has-failure');
-    $('#helpBlock').text("You must sign in to access that page.")
+    $('#helpBlock').text("You must sign in to access that page.");
   }
-  $( "#login-form" ).submit(function( event ) {
+  $("#login-form").submit(function(event) {
     event.preventDefault();
     var username = $('#login-form').find('input[id="inputUsername"]').val();
     var password = $('#login-form').find('input[id="inputPassword"]').val();
 
     $.ajax({
-       url: '/api/user/login',
-       data: {
-          "username": username,
-          "password": password,
-       },
-       success: function(data) {
-        if (data.status == 1){
-          localStorage.setItem("username",username);
+      url: '/api/user/login',
+      data: {
+        "username": username,
+        "password": password,
+      },
+      success: function(data) {
+        if (data.status == 1) {
+          localStorage.setItem("username", username);
           window.location.href = "/problems";
-        }
-        else{
+        } else {
           $('#helpBlock').removeClass("success-text");
           $('#inputGroup').addClass('has-failure');
           $('#helpBlock').text(data.message);
         }
-       },
-       type: 'POST'
+      },
+      type: 'POST'
     });
   });
 });
--- src/js/register.js
+++ src/js/register.js
@@ -5,203 +5,192 @@ function cleanStatus() {
 }
 
 function validateEmail(email) {
-    var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(email);
+  var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
 }
 
 function validatePassword() {
-    if($('#inputPassword').val().length < 6) {
-        $('#inputPassword').parent().parent().addClass('has-failure');
-        $('#inputPassword').parent().parent().removeClass('has-success');
-        $('#helpPassword').text('Passwords must be at least 6 characters');
-        return;
-	}
-	if($('#inputPassword').val().length > 50) {
-		$('#inputPassword').parent().parent().addClass('has-failure');
-		$('#inputPassword').parent().parent().removeClass('has-success');
-		$('#helpPassword').text('Passwords cannot be more than 50 characters');
-		return;
-	}
-	if($('#inputPassword').val() != $('#inputConfirmPassword').val()) {
-		$('#inputPassword').parent().parent().addClass('has-failure');
-		$('#inputPassword').parent().parent().removeClass('has-success');
-		$('#helpPassword').text('Passwords must match');
-		return;
-	}
-	$('#inputPassword').parent().parent().removeClass('has-failure');
-	$('#inputPassword').parent().parent().addClass('has-success');
-	$('#helpPassword').text('');
+  if ($('#inputPassword').val().length < 6) {
+    $('#inputPassword').parent().parent().addClass('has-failure');
+    $('#inputPassword').parent().parent().removeClass('has-success');
+    $('#helpPassword').text('Passwords must be at least 6 characters');
+    return;
+  }
+  if ($('#inputPassword').val().length > 50) {
+    $('#inputPassword').parent().parent().addClass('has-failure');
+    $('#inputPassword').parent().parent().removeClass('has-success');
+    $('#helpPassword').text('Passwords cannot be more than 50 characters');
+    return;
+  }
+  if ($('#inputPassword').val() != $('#inputConfirmPassword').val()) {
+    $('#inputPassword').parent().parent().addClass('has-failure');
+    $('#inputPassword').parent().parent().removeClass('has-success');
+    $('#helpPassword').text('Passwords must match');
+    return;
+  }
+  $('#inputPassword').parent().parent().removeClass('has-failure');
+  $('#inputPassword').parent().parent().addClass('has-success');
+  $('#helpPassword').text('');
 }
 
 function genericEmptyCheck(field, help) {
-	if(field.val().length == 0) {
-		field.parent().parent().addClass('has-failure');
-		field.parent().parent().removeClass('has-success');
-		help.text('This field is required');
-	}
-	else if(field.val().length > 32) {
-		field.parent().parent().addClass('has-failure');
-		field.parent().parent().removeClass('has-success');
-		help.text('No more than 32 characters are allowed');
-	}
-	else {
-		valid = true;
-		if(arguments.length > 2) {
-			var i;
-			for(i = 2; i < arguments.length; i++)
-			{
-				if (!arguments[i].hasClass('hidden') && arguments[i].val().length == 0) {
-					valid = false;
-					break;
-				}
-			}
-		}
-		if(valid) {
-			field.parent().parent().removeClass('has-failure');
-			field.parent().parent().addClass('has-success');
-		}
-		help.text('');
-	}
+  if (field.val().length == 0) {
+    field.parent().parent().addClass('has-failure');
+    field.parent().parent().removeClass('has-success');
+    help.text('This field is required');
+  } else if (field.val().length > 32) {
+    field.parent().parent().addClass('has-failure');
+    field.parent().parent().removeClass('has-success');
+    help.text('No more than 32 characters are allowed');
+  } else {
+    valid = true;
+    if (arguments.length > 2) {
+      var i;
+      for (i = 2; i < arguments.length; i++) {
+        if (!arguments[i].hasClass('hidden') && arguments[i].val().length == 0) {
+          valid = false;
+          break;
+        }
+      }
+    }
+    if (valid) {
+      field.parent().parent().removeClass('has-failure');
+      field.parent().parent().addClass('has-success');
+    }
+    help.text('');
+  }
 }
 
 function locationCheck() {
-	if($('#inputState').val().length > 32 || $('#inputCity').val().length > 32 || (!$('#inputInternational').hasClass('hidden') && $('#inputInternational').val().length > 32)) {
-		$('#inputState').parent().parent().addClass('has-failure');
-			$('#inputState').parent().parent().removeClass('has-success');
-			$('#helpLocation').text('These fields may only have up to 32 characters in them');
-			return;
-	}
-	if($('#inputInternational').hasClass('hidden')) {
-		if($('#inputState').val().length != 0 && $('#inputCity').val().length != 0) {
-			$('#inputState').parent().parent().removeClass('has-failure');
-			$('#inputState').parent().parent().addClass('has-success');
-			$('#helpLocation').text('');
-		}
-		else {
-			$('#inputState').parent().parent().addClass('has-failure');
-			$('#inputState').parent().parent().removeClass('has-success');
-			$('#helpLocation').text('Please enter a city and state');
-		}
-	}
-	else {
-		if($('#inputInternational').val().length != 0) {
-			$('#inputState').parent().parent().removeClass('has-failure');
-			$('#inputState').parent().parent().addClass('has-success');
-			$('#helpLocation').text('');
-		}
-		else
-		{
-			$('#inputState').parent().parent().addClass('has-failure');
-			$('#inputState').parent().parent().removeClass('has-success');
-			$('#helpLocation').text('Please enter a country');
-		}
-	}
+  if ($('#inputState').val().length > 32 || $('#inputCity').val().length > 32 || (!$('#inputInternational').hasClass('hidden') && $('#inputInternational').val().length > 32)) {
+    $('#inputState').parent().parent().addClass('has-failure');
+    $('#inputState').parent().parent().removeClass('has-success');
+    $('#helpLocation').text('These fields may only have up to 32 characters in them');
+    return;
+  }
+  if ($('#inputInternational').hasClass('hidden')) {
+    if ($('#inputState').val().length != 0 && $('#inputCity').val().length != 0) {
+      $('#inputState').parent().parent().removeClass('has-failure');
+      $('#inputState').parent().parent().addClass('has-success');
+      $('#helpLocation').text('');
+    } else {
+      $('#inputState').parent().parent().addClass('has-failure');
+      $('#inputState').parent().parent().removeClass('has-success');
+      $('#helpLocation').text('Please enter a city and state');
+    }
+  } else {
+    if ($('#inputInternational').val().length != 0) {
+      $('#inputState').parent().parent().removeClass('has-failure');
+      $('#inputState').parent().parent().addClass('has-success');
+      $('#helpLocation').text('');
+    } else {
+      $('#inputState').parent().parent().addClass('has-failure');
+      $('#inputState').parent().parent().removeClass('has-success');
+      $('#helpLocation').text('Please enter a country');
+    }
+  }
 }
 //Registration page submission
 $(function() {
-	$('#inputUsername').on('blur',function(e){
-	if($('#inputUsername').val().length < 3) {
-		$('#inputUsername').parent().parent().addClass('has-failure');
-		$('#inputUsername').parent().parent().removeClass('has-success');
-		$('#helpUsername').text('Usernames must be at least 3 characters');
-		return;
-	}
-	if($('#inputUsername').val().length > 32) {
-		$('#inputUsername').parent().parent().addClass('has-failure');
-		$('#inputUsername').parent().parent().removeClass('has-success');
-		$('#helpUsername').text('Usernames cannot be more than 32 characters');
-		return;
-	}
-    if(/\s/g.exec($('#inputUsername').val()) != null)
-    {
-        $('#inputUsername').parent().parent().addClass('has-failure');
-		$('#inputUsername').parent().parent().removeClass('has-success');
-		$('#helpUsername').text('Usernames may not contain whitespace');
-		return;
+  $('#inputUsername').on('blur', function(e) {
+    if ($('#inputUsername').val().length < 3) {
+      $('#inputUsername').parent().parent().addClass('has-failure');
+      $('#inputUsername').parent().parent().removeClass('has-success');
+      $('#helpUsername').text('Usernames must be at least 3 characters');
+      return;
+    }
+    if ($('#inputUsername').val().length > 32) {
+      $('#inputUsername').parent().parent().addClass('has-failure');
+      $('#inputUsername').parent().parent().removeClass('has-success');
+      $('#helpUsername').text('Usernames cannot be more than 32 characters');
+      return;
+    }
+    if (/\s/g.exec($('#inputUsername').val()) != null) {
+      $('#inputUsername').parent().parent().addClass('has-failure');
+      $('#inputUsername').parent().parent().removeClass('has-success');
+      $('#helpUsername').text('Usernames may not contain whitespace');
+      return;
     }
     $.ajax({
-       url: '/api/user/check_username',
-       data: {
-          "username": $('#inputUsername').val(),       },
-       success: function(data) {
-        if(data['status']==1) {
-			$('#inputUsername').parent().parent().addClass('has-success');
-			$('#inputUsername').parent().parent().removeClass('has-failure');
-			$('#helpUsername').text('');
-		}
-		else {
-			$('#inputUsername').parent().parent().addClass('has-failure');
-			$('#inputUsername').parent().parent().removeClass('has-success');
-			$('#helpUsername').text('That username is already taken!');
-		}
-       },
-       type: 'POST'
+      url: '/api/user/check_username',
+      data: {
+        "username": $('#inputUsername').val(),
+      },
+      success: function(data) {
+        if (data['status'] == 1) {
+          $('#inputUsername').parent().parent().addClass('has-success');
+          $('#inputUsername').parent().parent().removeClass('has-failure');
+          $('#helpUsername').text('');
+        } else {
+          $('#inputUsername').parent().parent().addClass('has-failure');
+          $('#inputUsername').parent().parent().removeClass('has-success');
+          $('#helpUsername').text('That username is already taken!');
+        }
+      },
+      type: 'POST'
     });
   });
 
-  $('#inputPassword').on('blur',function(e){
-	  validatePassword();
+  $('#inputPassword').on('blur', function(e) {
+    validatePassword();
   });
 
-    $('#inputConfirmPassword').on('blur',function(e){
-		validatePassword();
+  $('#inputConfirmPassword').on('blur', function(e) {
+    validatePassword();
   });
 
-  $('#inputFirstName').on('blur',function(e){
-	 genericEmptyCheck($('#inputFirstName'), $('#helpFirstName'), $('#inputLastName'));
+  $('#inputFirstName').on('blur', function(e) {
+    genericEmptyCheck($('#inputFirstName'), $('#helpFirstName'), $('#inputLastName'));
   });
 
-  $('#inputLastName').on('blur',function(e){
-	 genericEmptyCheck($('#inputLastName'), $('#helpLastName'), $('#inputFirstName'));
+  $('#inputLastName').on('blur', function(e) {
+    genericEmptyCheck($('#inputLastName'), $('#helpLastName'), $('#inputFirstName'));
   });
 
-  $('#inputAffiliation').on('blur',function(e){
-	 genericEmptyCheck($('#inputAffiliation'), $('#helpAffiliation'));
+  $('#inputAffiliation').on('blur', function(e) {
+    genericEmptyCheck($('#inputAffiliation'), $('#helpAffiliation'));
   });
 
   $('#inputStatus').on('blur', function(e) {
-	$('#inputStatus').parent().parent().addClass('has-success');
-});
+    $('#inputStatus').parent().parent().addClass('has-success');
+  });
 
   $('#inputEmail').on('blur', function(e) {
-  	if($('#inputEmail').val().length > 100) {
-  		$('#inputEmail').parent().parent().addClass('has-failure');
-  		$('#inputEmail').parent().parent().removeClass('has-success');
-  		$('#helpEmail').text('Invalid email address');
-  	}
-  	else if(validateEmail($('#inputEmail').val())){
-  		$('#inputEmail').parent().parent().addClass('has-success');
-  		$('#inputEmail').parent().parent().removeClass('has-failure');
-  		$('#helpEmail').text('');
-  	}
-  	else {
-  		$('#inputEmail').parent().parent().addClass('has-failure');
-  		$('#inputEmail').parent().parent().removeClass('has-success');
-  		$('#helpEmail').text('Invalid email address');
-  	}
+    if ($('#inputEmail').val().length > 100) {
+      $('#inputEmail').parent().parent().addClass('has-failure');
+      $('#inputEmail').parent().parent().removeClass('has-success');
+      $('#helpEmail').text('Invalid email address');
+    } else if (validateEmail($('#inputEmail').val())) {
+      $('#inputEmail').parent().parent().addClass('has-success');
+      $('#inputEmail').parent().parent().removeClass('has-failure');
+      $('#helpEmail').text('');
+    } else {
+      $('#inputEmail').parent().parent().addClass('has-failure');
+      $('#inputEmail').parent().parent().removeClass('has-success');
+      $('#helpEmail').text('Invalid email address');
+    }
   });
 
   $('#inputCountry').on('change', function(e) {
-  	if($('#inputCountry').val() == 'us') {
-  		$('#inputInternational').addClass('hidden');
-  		locationCheck();
-  	}
-  	else {
-  		$('#inputInternational').removeClass('hidden');
-  		locationCheck();
-  	}
+    if ($('#inputCountry').val() == 'us') {
+      $('#inputInternational').addClass('hidden');
+      locationCheck();
+    } else {
+      $('#inputInternational').removeClass('hidden');
+      locationCheck();
+    }
   });
   $('#inputCity').on('blur', function(e) {
-  		locationCheck();
+    locationCheck();
   });
   $('#inputState').on('blur', function(e) {
-  		locationCheck();
+    locationCheck();
   });
   $('#inputInternational').on('blur', function(e) {
-  		locationCheck();
+    locationCheck();
   });
 
-  $( "#registration-form" ).submit(function( event ) {
+  $("#registration-form").submit(function(event) {
     event.preventDefault();
     var username = $('#registration-form').find('input[id="inputUsername"]').val();
     var password = $('#registration-form').find('input[id="inputPassword"]').val();
@@ -225,52 +214,60 @@ $(function() {
     var selection = $('#registration-form').find('select[id="inputSection"]').val();
 
     var eligibility = "ineligible";
-	  cleanStatus();
-    if (country == "us" && schooltype == "high" && status == "comp"){
+    cleanStatus();
+    if (country == "us" && schooltype == "high" && status == "comp") {
       eligibility = "eligible";
     }
-	var formFailed = false;
-	$('.required').each(function() {
-		if($(this).hasClass('hidden')) return;
-		if(!$(this).parent().parent().hasClass('has-success')) {
-			formFailed = true;
-			console.log($(this));
-			if($(this).val().length == 0) {
-				$(this).parent().parent().addClass('has-failure');
-				$(this).parent().parent().removeClass('has-success');
-			}
-		}
-	});
+    var formFailed = false;
+    $('.required').each(function() {
+      if ($(this).hasClass('hidden')) return;
+      if (!$(this).parent().parent().hasClass('has-success')) {
+        formFailed = true;
+        console.log($(this));
+        if ($(this).val().length == 0) {
+          $(this).parent().parent().addClass('has-failure');
+          $(this).parent().parent().removeClass('has-success');
+        }
+      }
+    });
 
-    if (!formFailed){
+    if (!formFailed) {
       $.ajax({
-         url: '/api/user/create_simple',
-         data: {
-            "username": username,
-            "password": password,
-            "groupName": "",
-            "email": email,
-            "firstname": firstname,
-            "lastname": lastname,
-            "eligibility": eligibility,
-            "affiliation":affiliation,
-            "extra":JSON.stringify({"city":city,"state":state,"country":country, "international":international, "ctf":ctf,"experience":experience,"selection":selection,"status":status,"schooltype":schooltype}),
-						"g-recaptcha-response": grecaptcha.getResponse()
-				 },
-         success: function(data) {
-					if (data.status == 1){
-					 	window.location.href = "/login?v=1";
-					}
-					else{
+        url: '/api/user/create_simple',
+        data: {
+          "username": username,
+          "password": password,
+          "groupName": "",
+          "email": email,
+          "firstname": firstname,
+          "lastname": lastname,
+          "eligibility": eligibility,
+          "affiliation": affiliation,
+          "extra": JSON.stringify({
+            "city": city,
+            "state": state,
+            "country": country,
+            "international": international,
+            "ctf": ctf,
+            "experience": experience,
+            "selection": selection,
+            "status": status,
+            "schooltype": schooltype
+          }),
+          "g-recaptcha-response": grecaptcha.getResponse()
+        },
+        success: function(data) {
+          if (data.status == 1) {
+            window.location.href = "/login?v=1";
+          } else {
             $('#helpError').text("Error:" + data.message);
-						grecaptcha.reset()
-					}
-				 },
-         type: 'POST'
+            grecaptcha.reset()
+          }
+        },
+        type: 'POST'
       });
+    } else {
+      $('#helpError').text("Please enter all required information");
     }
-	else {
-		$('#helpError').text("Please enter all required information");
-	}
   });
 });
