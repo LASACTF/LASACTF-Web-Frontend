3de541ee9bf7ffb6b4d773717323d85a8a36341e
Oksisane <ehsanmasdar@gmail.com>
2016-03-08 20:49:10 -0600

    More fixes

--- src/js/problems.js
+++ src/js/problems.js
@@ -45,8 +45,10 @@ function ajaxSubmit(input, help, parent) {
             regenScoreboard(parseInt($('#textXP').text()) + parseInt(title.children('text').eq(1).text()));
             console.log( root.attr('data-category') );
             console.log(root.attr('data-percent'));
-            console.log(parseFloat($('#progressbar' + root.attr('data-category') + '.progress .progress-bar').width()) + root.attr('data-percent') + ' %' );
-            $('#progressbar' + root.attr('data-category') + ' .progress .progress-bar').width( parseFloat($('#progressbar' + root.attr('data-category') + ' .progress .progress-bar').width()) + root.attr('data-percent') + ' %' );
+            var currentwidth = parseFloat($('#progressbar' + root.attr('data-category') + ' .progress .progress-bar').width());
+            var updatedwidth = Math.min((currentwidth + parseFloat(root.attr('data-percent')))*100,100);
+            console.log(updatedwidth);
+            $('#progressbar' + root.attr('data-category') + ' .progress .progress-bar').width(updatedwidth);
 
           }, 1000);
       } else {
