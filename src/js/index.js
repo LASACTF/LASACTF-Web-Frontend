b5de04188e5c37c88ca3b982fa54e69d4919b2a3
Oksisane <ehsanmasdar@gmail.com>
2016-03-12 14:12:30 -0600

    Fix coutdown on index page

--- src/js/index.js
+++ src/js/index.js
@@ -4,18 +4,18 @@ function padDigits(number, digits) {
 
 if (Math.floor(Date.now() / 1000) < 1458406800) {
   var timespan = countdown(new Date("March 19, 2016 12:00:00 CDT"), function(ts) {
-    $('#tdays').text(padDigits(ts.days, 2));
-    $('#thours').text(padDigits(ts.hours, 2));
-    $('#tmins').text(padDigits(ts.minutes, 2));
-    $('#tsecs').text(padDigits(ts.seconds, 2));
+    $('#itdays').text(padDigits(ts.days, 2));
+    $('#ithours').text(padDigits(ts.hours, 2));
+    $('#itmins').text(padDigits(ts.minutes, 2));
+    $('#itsecs').text(padDigits(ts.seconds, 2));
   }, countdown.DAYS | countdown.HOURS | countdown.MINUTES | countdown.SECONDS | countdown.MILLISECONDS);
 } else {
-  $('#ttext').text('left');
+  $('#ittext').text('left');
   var timespan = countdown(new Date("March 26, 2016 12:00:00 CDT"), function(ts) {
-    $('#tdays').text(padDigits(ts.days, 2));
-    $('#thours').text(padDigits(ts.hours, 2));
-    $('#tmins').text(padDigits(ts.minutes, 2));
-    $('#tsecs').text(padDigits(ts.seconds, 2));
+    $('#itdays').text(padDigits(ts.days, 2));
+    $('#ithours').text(padDigits(ts.hours, 2));
+    $('#itmins').text(padDigits(ts.minutes, 2));
+    $('#itsecs').text(padDigits(ts.seconds, 2));
   }, countdown.DAYS | countdown.HOURS | countdown.MINUTES | countdown.SECONDS | countdown.MILLISECONDS);
 }
 $(function(){
